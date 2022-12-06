07327cb79adeae6963b49c2978890b573a289d4c
OumaimaRzini <119901030+OumaimaRzini@users.noreply.github.com>
2022-12-06 21:50:47 +0100

    Update exercice6.py

--- Atelier/exercice6.py
+++ Atelier/exercice6.py
@@ -1,9 +1,9 @@
-# compter nb des chiffres d'un nombre
+# compter les chiffres d'un nombre
 def compte(n):
     nb=0
     while(n!=0):
         n=n//10
-        nb+=1 # le nombre de quotient différent de 0
+        nb+=1 # le nombre de quotient est différent de 0
     return nb #condition d'arrêt n=0
 n=int(input("entrez un nombre:"))
 print("le nombre des chiffres de",n,"est",compte(n))
